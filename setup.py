8a3bd1de2e681e28623327b29879deac5b30b02c
zwb <zhuangweibin@stbl.cc>
2019-06-04 11:27:54 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zwb_data",
+    version="1.0.0",
+    keywords=("util"),
+    description="zwb_data",
+    long_description="zwb_data",
+    url="https://github.com/weibin268/zwb_data",
+    author="zhuang weibin",
+    author_email="448075543@qq.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
---
+++ tests/test_sqlalchemy.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestSQLAlchemy(unittest.TestCase):
+
+    def test_init(self):
+        print("test!!")
+
+
+if __name__ == '__main__':
+    unittest.main()

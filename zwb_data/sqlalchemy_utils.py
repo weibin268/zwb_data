a4ff12ea43100b91f9f6ca32efdb4e1ee98da829
weibin268 <binweizhuang@hotmail.com>
2019-06-07 20:26:06 +0800

    aa

--- zwb_data/sqlalchemy_utils.py
+++ zwb_data/sqlalchemy_utils.py
@@ -2,38 +2,27 @@ from sqlalchemy import Column, String, create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+connection_url = "mysql+mysqlconnector://root:Zwb@123456@198.12.97.201:3306/test"
+
 Base = declarative_base()
-
-default_session_key = "default_session_key"
-default_session_connection_string = "mysql+mysqlconnector://root:root@192.168.19.11:3306/demo"
-
-dict_sessionmaker = {}
+engine = create_engine(connection_url)
+Session = sessionmaker(bind=engine)
 
 
-def get_sessionmaker(*args, **kwargs):
-
-    dict_sessionmaker.get(default_session_connection_string)
-    engine = create_engine(*args, **kwargs)
+def init():
+    Base.metadata.create_all(engine)
 
 
 class User(Base):
     __tablename__ = 'user'
-
     id = Column(String(20), primary_key=True)
     name = Column(String(20))
 
 
-Base.metadata.create_all(engine)
-
-# 创建DBSession类型:
-Session = sessionmaker(bind=engine)
-print(type(Session))
-session = Session()
-print(type(session))
-user = User(id='5', name='Bob')
-# 添加到session:
-session.add(user)
-# 提交即保存到数据库:
-session.commit()
-# 关闭session:
-session.close()
+if __name__ == "__main__":
+        init()
+        session = Session()
+        user = User(id='1', name='Bob')
+        session.add(user)
+        session.commit()
+        session.close()
